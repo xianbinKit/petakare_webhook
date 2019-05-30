7bef115a4d9b136078685ff505d37e793ec14a8d
Xianbin Lin <lin_xianbin@icloud.com>
2019-05-30 18:18:15 +0200

    webhook init

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const PORT = 6474 // 需要打开AWS EC2对应端口
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+function promisifyCMD(cmd) {
+  const exec = require('child_process').exec
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        console.warn(error)
+      }
+      resolve(stdout ? stdout : stderr)
+    })
+  })
+}
+
+async function updateAPI(req, res, next) {
+  var content = req.body
+  console.log('body=>', content)
+  res.status(200).send('OK')
+}
+
+// Condiguration
+app.use(jsonParser)
+app.use(urlencodedParser)
+
+// webhook trigger
+app.use('/api', updateAPI)
+
+// Start server
+app.listen(PORT)
+console.log('app is listening on ', PORT)
